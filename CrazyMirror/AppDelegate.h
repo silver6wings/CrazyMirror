e836b7fc046eabb524c948de215cdc8b73616ab0
silver6wings <silver6wings@gmail.com>
2013-07-01 14:01:55 +0800

    Initial Commit

---
+++ CrazyMirror/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CrazyMirror
+//
+//  Created by Junchao Yu on 13-7-1.
+//  Copyright (c) 2013年 Junchao Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CrazyMirror/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrazyMirror
+//
+//  Created by Junchao Yu on 13-7-1.
+//  Copyright (c) 2013年 Junchao Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
